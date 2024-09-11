a81abaea795bab16214114dbad1e46587b752356
Alex Prucka <awp@prucka.com>
2024-09-10 19:03:47 -0500

    Lame Attempt

---
+++ GGOverlay/DatabaseHelper.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+public static class DatabaseHelper
+{
+    private const string DbFileName = "CounterDatabase.db";
+
+    public static void InitializeDatabase()
+    {
+        using (var connection = new SqliteConnection($"Data Source={DbFileName}"))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE IF NOT EXISTS Counter(
+                    ID INTEGER PRIMARY KEY,
+                    Value INTEGER NOT NULL
+                );
+                
+                INSERT INTO Counter (Value) 
+                SELECT 0 WHERE NOT EXISTS (SELECT 1 FROM Counter WHERE ID = 1);
+            ";
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public static int GetCounterValue()
+    {
+        using (var connection = new SqliteConnection($"Data Source={DbFileName}"))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT Value FROM Counter WHERE ID = 1;";
+            return Convert.ToInt32(command.ExecuteScalar());
+        }
+    }
+
+    public static void UpdateCounter(int newValue)
+    {
+        using (var connection = new SqliteConnection($"Data Source={DbFileName}"))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = "UPDATE Counter SET Value = @newValue WHERE ID = 1;";
+            command.Parameters.AddWithValue("@newValue", newValue);
+            command.ExecuteNonQuery();
+        }
+    }
+}
--- GGOverlay/MainWindow.xaml.cs
+++ GGOverlay/MainWindow.xaml.cs
@@ -1,135 +1,30 @@
-﻿using System.Net.Sockets;
-using System.Windows;
-using GGOverlay.Networking;
-using GGOverlay.Database;
+﻿using System.Windows;
 
-namespace GGOverlay
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        private NetworkServer _networkServer;
-        private NetworkClient _networkClient;
-        private DatabaseManager _databaseManager;
-        private Counters _counters;
+        InitializeComponent();
+        DatabaseHelper.InitializeDatabase();
+        UpdateCounterDisplay();
+    }
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            _databaseManager = new DatabaseManager("shared_data.db");
-            _networkServer = new NetworkServer(_databaseManager);
-            _networkClient = new NetworkClient(_databaseManager);
-            _counters = new Counters(_databaseManager); // Initialize the Counters class
+    private void Increment_Click(object sender, RoutedEventArgs e)
+    {
+        var currentValue = DatabaseHelper.GetCounterValue();
+        DatabaseHelper.UpdateCounter(currentValue + 1);
+        UpdateCounterDisplay();
+    }
 
-            _networkServer.OnLogMessage += Log;
-            _networkClient.OnDataReceived += DataReceived;
+    private void Decrement_Click(object sender, RoutedEventArgs e)
+    {
+        var currentValue = DatabaseHelper.GetCounterValue();
+        DatabaseHelper.UpdateCounter(currentValue - 1);
+        UpdateCounterDisplay();
+    }
 
-
-
-            // Subscribe to database change events to update the UI
-            _databaseManager.OnDatabaseChanged += UpdateUIFromDatabaseChange;
-        }
-
-        private void HostButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Ensure counters exist in the database, creating them if necessary
-            _counters.InitializeCounters(); // Ensure this method is public in Counters.cs
-
-            // Load initial state from the database
-            LoadCounterValues();
-
-            _networkServer.StartServer();
-            Log("Server started... Waiting for clients.");
-        }
-
-        private void JoinButton_Click(object sender, RoutedEventArgs e)
-        {
-            string ipAddress = IpAddressTextBox.Text.Trim();
-            if (!string.IsNullOrEmpty(ipAddress))
-            {
-                _networkClient.ConnectToServer(ipAddress);
-                Log("Connecting to server...");
-            }
-            else
-            {
-                Log("Please enter a valid IP address.");
-            }
-        }
-
-        private void Counter1PlusButton_Click(object sender, RoutedEventArgs e)
-        {
-            int currentValue = _counters.GetCounterValue("Counter1");
-            _counters.UpdateCounterValue("Counter1", currentValue + 1);
-        }
-
-        private void Counter1MinusButton_Click(object sender, RoutedEventArgs e)
-        {
-            int currentValue = _counters.GetCounterValue("Counter1");
-            _counters.UpdateCounterValue("Counter1", currentValue - 1);
-        }
-
-        private void Counter2PlusButton_Click(object sender, RoutedEventArgs e)
-        {
-            int currentValue = _counters.GetCounterValue("Counter2");
-            _counters.UpdateCounterValue("Counter2", currentValue + 1);
-        }
-
-        private void Counter2MinusButton_Click(object sender, RoutedEventArgs e)
-        {
-            int currentValue = _counters.GetCounterValue("Counter2");
-            _counters.UpdateCounterValue("Counter2", currentValue - 1);
-        }
-
-        private void Counter3PlusButton_Click(object sender, RoutedEventArgs e)
-        {
-            int currentValue = _counters.GetCounterValue("Counter3");
-            _counters.UpdateCounterValue("Counter3", currentValue + 1);
-        }
-
-        private void Counter3MinusButton_Click(object sender, RoutedEventArgs e)
-        {
-            int currentValue = _counters.GetCounterValue("Counter3");
-            _counters.UpdateCounterValue("Counter3", currentValue - 1);
-        }
-
-        // Update UI elements when database changes occur
-        private void UpdateUIFromDatabaseChange(string changeMessage)
-        {
-            Application.Current.Dispatcher.Invoke(() => LoadCounterValues());
-        }
-
-        // Load counter values from the database into the UI
-        private void LoadCounterValues()
-        {
-            Counter1TextBox.Text = _counters.GetCounterValue("Counter1").ToString();
-            Counter2TextBox.Text = _counters.GetCounterValue("Counter2").ToString();
-            Counter3TextBox.Text = _counters.GetCounterValue("Counter3").ToString();
-        }
-
-        private void Log(string message)
-        {
-            if (LogTextBox.Dispatcher.CheckAccess())
-            {
-                LogTextBox.AppendText($"{message}\n");
-                LogTextBox.ScrollToEnd();
-            }
-            else
-            {
-                LogTextBox.Dispatcher.Invoke(() =>
-                {
-                    LogTextBox.AppendText($"{message}\n");
-                    LogTextBox.ScrollToEnd();
-                });
-            }
-        }
-
-        private void ClientConnected(TcpClient client)
-        {
-            Log("Client connected.");
-        }
-
-        private void DataReceived(string message)
-        {
-            Log($"Received: {message}");
-        }
+    private void UpdateCounterDisplay()
+    {
+        lblCounter.Content = "Counter: " + DatabaseHelper.GetCounterValue();
     }
 }
